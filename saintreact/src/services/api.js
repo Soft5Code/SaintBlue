b2a26775136053c3dae198c7cd3a6fb792c0530f
dillow191 <danieldoskil@gmail.com>
2024-11-24 16:08:41 -0300

    Tentando fazer o vercel funcionar

--- saintreact/src/services/api.js
+++ saintreact/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"/api",
+    baseURL:"http://ec2-34-224-23-65.compute-1.amazonaws.com:5000/estoque/",
     headers: {
         "Content-Type": "application/json"
     },
